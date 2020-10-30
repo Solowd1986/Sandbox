a35bf98d86543145cceb4ff80de8ce8cfbcc3fbb
Admin <Admin@ya.com>
2020-10-31 02:59:25 +0300

    f1

--- src/components/Pages/Order/OrderInfo/OrderInfo.js
+++ src/components/Pages/Order/OrderInfo/OrderInfo.js
@@ -4,44 +4,45 @@ import styles from "./order-info.module.scss";
 export default class OrderInfo extends Component {
     render() {
         return (
-            <div className="basket-info">
-                <section className="basket-delivery">
-                    <h2 className="basket-order-title">1. Доставка</h2>
-                    <div className="basket-cards-wrapper">
-                        <div className="basket-card basket-card--active">
-                            <div className="basket-card__info">
+            <section className={styles.info}>
+                {/*Delivery*/}
+                <div>
+                    <h2 className={styles.order_title}>1. Доставка</h2>
+                    <div className={styles.cards_wrapper}>
+                        <div className={`${styles.card} ${styles.card__active}`}>
+                            <div className={styles.card__info}>
                                 <span> Доставка по Москве</span>
-                                <span className="basket-card__pay">бесплатно</span>
+                                <span className={styles.card__pay}>бесплатно</span>
                             </div>
-                            <div className="basket-card__extra">сегодня</div>
+                            <div className={styles.card__extra}>сегодня</div>
                         </div>
 
-                        <div className="basket-card">
-                            <div className="basket-card__info">
+                        <div className={styles.card}>
+                            <div className={styles.card__info}>
                                 <span>Самовывоз</span>
-                                <span className="basket-card__pay">бесплатно</span>
+                                <span className={styles.card__pay}>бесплатно</span>
                             </div>
-                            <div className="basket-card__extra">Москва, Барклая 6, стр. 5, БЦ "Барклай Плаза" (м. Парк Победы)</div>
+                            <div className={styles.card__extra}>Москва, Барклая 6, стр. 5, БЦ "Барклай Плаза" (м. Парк Победы)</div>
                         </div>
 
-                        <div className="basket-card">
-                            <div className="basket-card__info">
+                        <div className={styles.card}>
+                            <div className={styles.card__info}>
                                 <span>Доставка по России</span>
-                                <span className="basket-card__pay">бесплатно</span>
+                                <span className={styles.card__pay}>бесплатно</span>
                             </div>
-                            <div className="basket-card__extra">
-                                <img src="/src/assets/img/other/basket-shipping.png" alt="shipping"/>
+                            <div className={styles.card__extra}>
+                                <img src="img/other/basket-shipping.png" alt="shipping"/>
                             </div>
                         </div>
                     </div>
-                </section>
+                </div>
 
-                <section className="basket-customer">
-                    <h2 className="basket-order-title">2. Покупатель</h2>
-                    <div className="basket-form-user-data">
-
-                        <label className="basket-form__label">
-                            <input className="basket-form__input"
+                {/*Customer*/}
+                <div className={styles.customer}>
+                    <h2 className={styles.order_title}>2. Покупатель</h2>
+                    <div className={styles.form_user_data}>
+                        <label className={styles.form__label}>
+                            <input className={styles.form__input}
                                    name="customer-name"
                                    type="text"
                                    maxLength="255"
@@ -51,8 +52,8 @@ export default class OrderInfo extends Component {
                                    required/>
                         </label>
 
-                        <label className="basket-form__label">
-                            <input className="basket-form__input"
+                        <label className={styles.form__label}>
+                            <input className={styles.form__input}
                                    name="customer-phone"
                                    type="text"
                                    maxLength="255"
@@ -62,8 +63,8 @@ export default class OrderInfo extends Component {
                                    required/>
                         </label>
 
-                        <label className="basket-form__label">
-                            <input className="basket-form__input"
+                        <label className={styles.form__label}>
+                            <input className={styles.form__input}
                                    name="customer-email"
                                    type="email"
                                    maxLength="255"
@@ -73,53 +74,53 @@ export default class OrderInfo extends Component {
                                    required/>
                         </label>
 
-                        <label className="basket-form__label basket-form__label--full-width">
-                            <input
-                                className="basket-form__input"
-                                name="customer-address"
-                                type="text"
-                                maxLength="255"
-                                minLength="3"
-                                autoComplete="on"
-                                placeholder="Адрес"
-                                required/>
+                        <label className={`${styles.form__label} ${styles.form__label__full_width}`}>
+                            <input className={styles.form__input}
+                                   name="customer-address"
+                                   type="text"
+                                   maxLength="255"
+                                   minLength="3"
+                                   autoComplete="on"
+                                   placeholder="Адрес"
+                                   required/>
                         </label>
 
-                        <label className="basket-form__label basket-form__label--full-width">
-                                               <textarea
-                                                   className="basket-form__input"
-                                                   name="customer-comment"
-                                                   id="" cols="30"
-                                                   rows="10"
-                                                   placeholder="Комментарий"/>
+                        <label className={`${styles.form__label} ${styles.form__label__full_width}`}>
+                            <textarea className={styles.form__input}
+                                      name="customer-comment"
+                                      id="" cols="30"
+                                      rows="10"
+                                      placeholder="Комментарий"/>
                         </label>
                     </div>
-                </section>
-                <section className="basket-checkout">
-                    <h2 className="basket-order-title">3. Оплата</h2>
-                    <div className="basket-cards-wrapper">
-                        <div className="basket-card basket-card--active">
-                            <p className="basket-card__info">
-                                <span className="basket-card__title">Оплата наличными</span>
+                </div>
+
+                {/*Checkout*/}
+                <div>
+                    <h2 className={styles.order_title}>3. Оплата</h2>
+                    <div className={styles.cards_wrapper}>
+                        <div className={`${styles.card} ${styles.card__active}`}>
+                            <p className={styles.card__info}>
+                                <span className={styles.card__title}>Оплата наличными</span>
                             </p>
                         </div>
-                        <div className="basket-card">
-                            <p className="basket-card__info">
-                                <span className="basket-card__title">Электронными деньгами</span>
-                                <span className="basket-card__pay">бесплатно</span>
+                        <div className={styles.card}>
+                            <p className={styles.card__info}>
+                                <span className={styles.card__title}>Электронными деньгами</span>
+                                <span className={styles.card__pay}>бесплатно</span>
                             </p>
-                            <span className="basket-card__epayment">
-                                                    <img src="/src/assets/img/other/basket-epayment.png" alt="epayment"/>
-                                                </span>
+                            <span>
+                                <img src="img/other/basket-epayment.png" alt="epayment"/>
+                            </span>
                         </div>
-                        <div className="basket-card ">
-                            <p className="basket-card__info">
-                                <span className="basket-card__title">Безналичный расчет (для юр. лиц)</span>
+                        <div className={styles.card}>
+                            <p className={styles.card__info}>
+                                <span className={styles.card__title}>Безналичный расчет (для юр. лиц)</span>
                             </p>
                         </div>
                     </div>
-                </section>
-            </div>
+                </div>
+            </section>
         )
     }
 }
--- src/components/Pages/Order/OrderSummary/OrderSummary.js
+++ src/components/Pages/Order/OrderSummary/OrderSummary.js
@@ -4,54 +4,55 @@ import styles from "./order-summary.module.scss";
 export default class OrderSummary extends Component {
     render() {
         return (
-            <div className="basket-summary">
-                <h2 className="basket-summary__caption">Ваш заказ</h2>
-                <div className="basket-summary__item">
-                    <div className="basket-summary__info">
-                        <img className="basket-summary__img-sm" src="assets/img/oneplus_7pro-basket.png" alt="image"/>
-                        <div className="basket-summary__info-inner-wrapper">
-                            <p className="basket-summary__product-title">Oneplus 7 Pro 12GB + 256GB (туманный синий)</p>
+            <section className={styles.summary}>
+                <h2 className={styles.caption}>Ваш заказ</h2>
+                <div className={styles.item}>
+                    <div className={styles.info}>
+                        <img className={styles.img_sm} src="img/oneplus_7pro-basket.png" alt="image"/>
+                        <div className={styles.info_inner_wrapper}>
+                            <p className={styles.product_title}>Oneplus 7 Pro 12GB + 256GB (туманный синий)</p>
                         </div>
-                        <span className="basket-summary__price--sm">44 000 р.</span>
+                        <span className={styles.price__sm}>44 000 р.</span>
                     </div>
 
-                    <div className="basket-summary__counter-block">
-                        <span className="basket-summary__counter basket-summary__counter-minus"/>
-                        <label><input className="basket-summary__counter-value" type="text" name="customer-product-count" value="1"/></label>
-                        <span className="basket-summary__counter basket-summary__counter-plus"/>
-                    </div>
-                </div>
-                <div className="basket-summary__item">
-                    <div className="basket-summary__info">
-                        <img className="basket-summary__img-sm" src="assets/img/oneplus_8pro-basket.png" alt="image"/>
-                        <div className="basket-summary__info-inner-wrapper">
-                            <p className="basket-summary__product-title">Oneplus 8 Pro 12GB + 256GB (ледяной зеленый)</p>
-                        </div>
-                        <span className="basket-summary__price--sm">74 000 р.</span>
-                    </div>
-                    <div className="basket-summary__counter-block">
-                        <span className="basket-summary__counter basket-summary__counter-minus"/>
-                        <label><input className="basket-summary__counter-value" type="text" name="customer-product-count" value="1"/></label>
-                        <span className="basket-summary__counter basket-summary__counter-plus"/>
+                    <div className={styles.counter_block}>
+                        <span className={`${styles.counter} ${styles.counter_minus}`}/>
+                        <label><input type="text" name="customer-product-count" value="1"/></label>
+                        <span className={`${styles.counter} ${styles.counter_plus}`}/>
                     </div>
                 </div>
 
-                <div className="basket-summary__delivery-block">
-                    <div className="basket-summary__delivery-fieldset">
-                        <span className="basket-summary__delivery-item">Сумма:</span>
-                        <span className="basket-summary__delivery-item">44 000 р.</span>
+                <div className={styles.item}>
+                    <div className={styles.info}>
+                        <img className={styles.img_sm} src="img/oneplus_8pro-basket.png" alt="image"/>
+                        <div className={styles.info_inner_wrapper}>
+                            <p className={styles.product_title}>Oneplus 8 Pro 12GB + 256GB (ледяной зеленый)</p>
+                        </div>
+                        <span className={styles.price__sm}>74 000 р.</span>
                     </div>
-                    <div className="basket-summary__delivery-fieldset">
-                        <span className="basket-summary__delivery-item">Доставка по Москве:</span>
-                        <span className="basket-summary__delivery-item">0 р.</span>
+
+                    <div className={styles.counter_block}>
+                        <span className={`${styles.counter} ${styles.counter_minus}`}/>
+                        <label><input type="text" name="customer-product-count" value="1"/></label>
+                        <span className={`${styles.counter} ${styles.counter_plus}`}/>
                     </div>
                 </div>
-                <div className="basket-summary__checkout">
-                    <span className="basket-summary__delivery-item">Итого:</span>
-                    <span className="basket-summary__price--lg">44 000 р.</span>
+
+                <div className={styles.delivery_fieldset}>
+                    <span className={styles.delivery_item}>Сумма:</span>
+                    <span className={styles.delivery_item}>44 000 р.</span>
                 </div>
-                <button className="btn btn-basket-order">Оформить заказ</button>
-            </div>
+                <div className={styles.delivery_fieldset}>
+                    <span className={styles.delivery_item}>Доставка по Москве:</span>
+                    <span className={styles.delivery_item}>0 р.</span>
+                </div>
+
+                <div className={styles.checkout}>
+                    <span className={styles.delivery_item}>Итого:</span>
+                    <span className={styles.price__lg}>44 000 р.</span>
+                </div>
+                <button className={`btn ${styles.order_btn}`}>Оформить заказ</button>
+            </section>
 
         )
     }
