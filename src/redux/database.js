8ea893d8157a3b3dc9a6a17b579453266e2e6b99
Admin <Admin@ya.com>
2020-11-13 07:12:20 +0300

    f1

---
+++ src/redux/database.js
@@ -0,0 +1,138 @@
+const dataList = {
+
+    phones: {
+        categoryTitle: "Смартфоны",
+        productList: [
+            {
+                id: 1,
+                title: "Oneplus 6T 6GB + 128GB",
+                color: "(черный оникс)",
+                price: 44500,
+                rest: 100,
+                type: "",
+
+
+                imgPath: "main-page/phone-main-page/oneplus_6t_6gb_128gb_black_275_1.png",
+                imgAlt: "promo-phone-image",
+                discount: true
+            },
+            {
+                id: 2,
+                title: "Oneplus 6T 8GB + 128GB",
+                color: "(синий ультрамарин)",
+                price: 61000,
+                rest: 60,
+                imgPath: "main-page/phone-main-page/oneplus_6t_8gb_128gb_purple_275_1.png",
+                imgAlt: "promo-phone-image",
+                discount: false
+            },
+            {
+                id: 3,
+                title: "Oneplus 7 Pro 8GB + 256GB",
+                color: "(дымчато красный)",
+                price: 51000,
+                rest: 34,
+                imgPath: "main-page/phone-main-page/oneplus_7_8gb_256gb_red_275_1.png",
+                imgAlt: "promo-phone-image",
+                discount: true
+            },
+            {
+                id: 4,
+                title: "Oneplus 7 Pro 12GB + 256GB",
+                color: "(зеркальный серый)",
+                price: 76000,
+                rest: 32,
+                imgPath: "main-page/phone-main-page/oneplus_7_12gb_256gb_grey_275_1.png",
+                imgAlt: "promo-phone-image",
+                discount: true
+            },
+        ],
+    },
+
+    accessoires: {
+        categoryTitle: "Аксессуары",
+        productList: [
+            {
+                id: 11,
+                title: "Беспроводные наушники OnePlus Bullets Wireless",
+                price: 12000,
+                rest: 12,
+                imgPath: "main-page/accessoires-main-page/oneplus_bullets_wireless_275_1.png",
+                imgAlt: "promo-accessoires-image",
+                discount: false
+            },
+            {
+                id: 21,
+                title: "Адаптер OnePlus Dash Power",
+                price: 6000,
+                rest: 42,
+                imgPath: "main-page/accessoires-main-page/oneplus_dash_charger_275_1.png",
+                imgAlt: "promo-accessoires-image",
+                discount: false
+            },
+            {
+                id: 34,
+                title: "Автомобильное зарядное устройство OnePlus Warp Charge",
+                price: 9800,
+                rest: 17,
+                imgPath: "main-page/accessoires-main-page/oneplus_warp_charge_30_275_1.png",
+                imgAlt: "promo-accessoires-image",
+                discount: true
+            },
+            {
+                id: 151,
+                title: "Адаптер OnePlus Type-C - 3.5мм",
+                price: 2000,
+                rest: 52,
+                imgPath: "main-page/accessoires-main-page/oth_cable_oneplus_type_c_275_1.png",
+                imgAlt: "promo-accessoires-image",
+                discount: false
+            }
+        ],
+    },
+
+    gadgets: {
+        categoryTitle: "Гаджеты",
+        productList: [
+            {
+                id: 566,
+                title: "Зубная электрощетка Soocas X3 Sonic Electric ToothBrush",
+                price: 7900,
+                rest: 32,
+                imgPath: "main-page/gadgets-main-page/electric_tooth_brush_275_1.png",
+                imgAlt: "promo-gadgets-image",
+                discount: false
+            },
+            {
+                id: 766,
+                title: "Робот-пылесос Roborock Sweep One",
+                price: 17200,
+                rest: 52,
+                imgPath: "main-page/gadgets-main-page/roborock_sweep_one_275_1.png",
+                imgAlt: "promo-gadgets-image",
+                discount: true
+            },
+            {
+                id: 567,
+                title: "Автомобильное зарядное устройство ZMI Car Charger AP821",
+                price: 11500,
+                rest: 78,
+                imgPath: "main-page/gadgets-main-page/digital_display_car_charger_275_1.png",
+                imgAlt: "promo-gadgets-image",
+                discount: false
+            },
+            {
+                id: 311,
+                title: "Зеркало для макияжа Amiro Lux High Color",
+                price: 15000,
+                rest: 7,
+                imgPath: "main-page/gadgets-main-page/amiro_lux_high_color_275_1.png",
+                imgAlt: "promo-gadgets-image",
+                discount: false
+            },
+        ],
+    },
+
+
+    slider: {},
+};
