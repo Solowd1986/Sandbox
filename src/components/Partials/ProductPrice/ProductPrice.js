8acb00fa0b3b7282409927a92aeda387c243a4dd
Admin <Admin@ya.com>
2021-03-17 05:59:33 +0300

    f1

--- src/components/Partials/ProductPrice/ProductPrice.js
+++ src/components/Partials/ProductPrice/ProductPrice.js
@@ -8,6 +8,8 @@ import cn from "classnames";
  * при попытке обратиться к свойству от null или undeifned (то есть если бы classList = null, например), по сути условие
  * всегда сработает, так как classList всегда true при указанном подходе, но вместо класса подставится "", если ничего не
  * передано  так что не проблема. А если набор классов все же придет, то они без проблем извлекутся и подставятся.
+ *
+ * В конце округляем результат преобразования цены до сотен
  */
     //endregion
 const ProductPrice = ({ product: { price, discount, rest }, classList = { main: "", discount: "" } }) => {
@@ -21,17 +23,18 @@ const ProductPrice = ({ product: { price, discount, rest }, classList = { main: 
         });
 
         let initialPrice = discount ? <span className={initialPriceClassList}>{formatPrice(price)} р.</span> : null;
-        let finalPrice = discount ? formatPrice(price - (price * 10 / 100)) + " р." : formatPrice(price) + " р.";
+        let finalPrice = discount
+            ? formatPrice(Math.round((price - (price * 10 / 100)) / 100 * 100)) + " р."
+            : formatPrice(price) + " р.";
 
         if (!rest) return null;
-    return (
-        <span className={finalPriceClassList}>
-            {initialPrice}
-            {finalPrice}
-        </span>
-    )
+        return (
+            <span className={finalPriceClassList}>
+                {initialPrice}
+                {finalPrice}
+            </span>
+        )
 };
-
 
 export default ProductPrice;
 
